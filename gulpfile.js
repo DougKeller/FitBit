a0a770b89a72476edeec4a9954b785e1eb19a9e8
DougKeller <dwk24@zips.uakron.edu>
2015-11-15 13:44:51 -0500

    Explicitly minify/join depending on task

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,53 @@
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
 var concat = require('gulp-concat');
-var gulpif = require('gulp-if');
 var uglify = require('gulp-uglify');
 var minifyCss = require('gulp-minify-css');
-var args = require('yargs').argv;
 
-gulp.task('js', function() {
+gulp.task('join_js', function() {
   gulp.src(['!public/javascripts/vendor/**/*.js', 'public/javascripts/**/app.js', 'public/javascripts/**/*.js'])
     .pipe(concat('/public/application.js'))
-    .pipe(gulpif(!args.dev, uglify()))
     .pipe(gulp.dest('.'))
   gulp.src(['public/javascripts/vendor/**/*.js'])
     .pipe(concat('/public/vendor.js'))
-    .pipe(gulpif(!args.dev, uglify()))
     .pipe(gulp.dest('.'))
 })
 
-gulp.task('css', function() {
+gulp.task('join_css', function() {
   gulp.src(['public/stylesheets/**/imports.css', 'public/stylesheets/**/*.css'])
     .pipe(concat('/public/style.css'))
-    .pipe(gulpif(!args.dev, minifyCss()))
+    .pipe(gulp.dest('.'))
+})
+
+gulp.task('minify_js', function() {
+  gulp.src(['!public/javascripts/vendor/**/*.js', 'public/javascripts/**/app.js', 'public/javascripts/**/*.js'])
+    .pipe(concat('/public/application.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('.'))
+  gulp.src(['public/javascripts/vendor/**/*.js'])
+    .pipe(concat('/public/vendor.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('.'))
+})
+
+gulp.task('minify_css', function() {
+  gulp.src(['public/stylesheets/**/imports.css', 'public/stylesheets/**/*.css'])
+    .pipe(concat('/public/style.css'))
+    .pipe(minifyCss())
     .pipe(gulp.dest('.'))
 })
 
 gulp.task('dev', function() {
-  args.dev = true
-  gulp.start('js', 'css')
+  gulp.start('join_js', 'join_css')
   nodemon({
     script: 'app.js',
     ext: 'js css',
     ignore: ['public/application.js', 'public/style.css', 'public/vendor.js'],
-    tasks: ['js', 'css']
+    tasks: ['join_js', 'join_css'],
+    args: ['dev']
   })
 })
 
 gulp.task('build', function() {
-  gulp.start('js', 'css')
+  gulp.start('minify_js', 'minify_css')
 })
